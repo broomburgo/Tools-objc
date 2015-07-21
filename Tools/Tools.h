7e4d2c33e72d83f41a62010f80db1b549ccc3bb8
Elviro Rocca <elviro.rocca@facile.it>
2015-07-21 08:26:54 +0200

    Initial Commit

---
+++ Tools/Tools.h
@@ -0,0 +1,13 @@
+//
+//  Tools.h
+//  Tools
+//
+//  Created by Elviro Rocca on 21/07/15.
+//  Copyright (c) 2015 Elvi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Tools : NSObject
+
+@end
---
+++ Tools/Tools.m
@@ -0,0 +1,13 @@
+//
+//  Tools.m
+//  Tools
+//
+//  Created by Elviro Rocca on 21/07/15.
+//  Copyright (c) 2015 Elvi. All rights reserved.
+//
+
+#import "Tools.h"
+
+@implementation Tools
+
+@end
